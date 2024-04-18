e122e99fd0ed27a5e51ea833483918c1f7ebbd5d
Liudmyla Bolotova <liudmylabolotova@gmail.com>
2024-04-19 01:28:20 +0300

    added new files, no finished work

---
+++ src/app/(server)/api.ts
@@ -0,0 +1,9 @@
+// import lessons from "../lessons.json";
+
+// export function getArticleById(id: number) {
+//   const article = lessons.lectures.find(
+//     (lecture) => lecture.id === id
+//   );
+
+//  console.log(article)
+// }
--- src/app/articles/[articleName]/page.tsx
+++ src/app/articles/[articleName]/page.tsx
@@ -1,3 +1,26 @@
-export default function ArticlePage() {
-    return <>This is article</>
+// import { getArticleById } from '@/app/(server)/api';
+
+import lessons from "../../lessons.json";
+
+export function getArticleById(id: number) {
+  const article = lessons.lectures.find(
+    (lecture) => lecture.id === id
+  );
+
+  if (!article) {
+    throw new Error("Error ONE");
+  }
+
+ return article;
 }
+
+export default async function ArticlePage({ params }: Params) {
+    const { id } = params;
+    const article = await getArticleById(id);
+
+    if (!article) {
+        throw new Error("Error!!!!");
+      }
+  
+    console.log(article)
+  }
