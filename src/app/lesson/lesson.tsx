4f8feaa0aea7bb7ab1ef4b4abeb208ffcc953d4e
Liudmyla Bolotova <liudmylabolotova@gmail.com>
2024-05-03 23:55:36 +0300

    fixed styles

--- src/app/lesson/lesson.tsx
+++ src/app/lesson/lesson.tsx
@@ -10,7 +10,6 @@ export function Lesson({ title, lecturer, description }: LessonProps) {
   return (
     <div>
       <h3 className={styles.headerMiddle}>{title}</h3>
-
       <h4 className={styles.headerSmall}>{lecturer}</h4>
       <p>{description}</p>
     </div>
