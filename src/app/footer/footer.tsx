b71dd6507fd1dfa79ee00f957bf7105b2ce3bebc
Liudmyla Bolotova <liudmylabolotova@gmail.com>
2024-05-07 20:54:09 +0300

    fixed global style and footer

--- src/app/footer/footer.tsx
+++ src/app/footer/footer.tsx
@@ -1,9 +1,14 @@
 import styles from "./footer.module.css";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className={styles.sectionFooter}>
-      <p>Write to us!</p>
+      <address className={styles.sectionAdress}>
+        <Link href="mailto:email@example.com">email@example.com</Link>
+        <Link href="tel:+1234567890">+1 (234) 567-890</Link>
+      </address>
+
       <div className={styles.sectionIcons}>
         <svg width="26" height="26">
           <use href="/assets/icons.svg#icon-linkedin"></use>
