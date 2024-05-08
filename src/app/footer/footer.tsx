c886cef553d9fad8875509db7cc3b6adc6596495
Liudmyla Bolotova <liudmylabolotova@gmail.com>
2024-05-08 19:10:54 +0300

    fixed footer and styles

--- src/app/footer/footer.tsx
+++ src/app/footer/footer.tsx
@@ -1,13 +1,15 @@
 import styles from "./footer.module.css";
-import Link from "next/link";
+import {AppLink} from "../app-link/app-link";
 
 export default function Footer() {
   return (
     <div className={styles.sectionFooter}>
-      <address className={styles.sectionAdress}>
-        <Link href="mailto:email@example.com">email@example.com</Link>
-        <Link href="tel:+1234567890">+1 (234) 567-890</Link>
-      </address>
+      <div className={styles.sectionAdress}>
+        <h5>Write to us</h5>
+        <AppLink href="mailto:email@example.com">email@example.com</AppLink>
+        <h5>Call us</h5>
+        <AppLink href="tel:+1234567890">+1 (234) 567-890</AppLink>
+      </div>
 
       <div className={styles.sectionIcons}>
         <svg width="26" height="26">
